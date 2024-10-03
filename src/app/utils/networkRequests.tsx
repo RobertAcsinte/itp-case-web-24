f5893c35203742bc8c57ea318a63a40081da54fc
Robert Acsinte <robert.acsinte@gmail.com>
2024-10-04 01:01:05 +0200

    network request function

---
+++ src/app/utils/networkRequests.tsx
@@ -0,0 +1,10 @@
+export async function getData<T>(url: string): Promise<T> {
+
+      const response = await fetch(url, { cache: 'no-store' });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      return json;
+  }
+  
