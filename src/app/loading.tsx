291e9d1233382a39a406845daf80c0ca84758d91
Robert Acsinte <robert.acsinte@gmail.com>
2024-10-04 01:47:27 +0200

    loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { BeatLoader } from "react-spinners";
+
+export default function Loading() {
+    return (
+        <main className="wrapper-center">
+            <BeatLoader />
+        </main>
+    )
+  }
