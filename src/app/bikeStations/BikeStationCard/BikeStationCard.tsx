44e8c70477623cf5662c8f689ae7983b4d26bc69
Robert Acsinte <robert.acsinte@gmail.com>
2024-10-04 18:52:42 +0200

    copy to clipboard functionality

--- src/app/bikeStations/BikeStationCard/BikeStationCard.tsx
+++ src/app/bikeStations/BikeStationCard/BikeStationCard.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import style from './BikeStationCard.module.scss';
 
 type Station = {
@@ -8,12 +11,23 @@ type Station = {
 }
 
 export default function BikeStationCard({station}: {station: Station}) {
+    const [isCopied, setIsCopied] = useState(false);
+
+    const handleCopy = async () => {
+        try {
+          await navigator.clipboard.writeText(station.name); 
+          setIsCopied(true);
+          setTimeout(() => setIsCopied(false), 2000);
+        } catch (err) {
+          console.error("Failed to copy text:", err);
+        }
+      };
     
     return (
         <div className={style.container}>
             <div className={style.titleWrapper}>
                 <h2>{station.name}</h2>
-                <button>Copy to clipboard</button>
+                <button onClick={() => handleCopy()}>{isCopied ? 'Copied!' : 'Copy to clipboard'}</button>
             </div>
             <p><b>Bikes available:</b> {station.bikes_available}</p>
             <p><b>Total bikes:</b> {station.bikes_in_use + station.bikes_available}</p>
