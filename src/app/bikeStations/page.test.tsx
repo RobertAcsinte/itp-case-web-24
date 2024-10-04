78668acee9d01fcf85594b1107a4d96be5765de5
Robert Acsinte <robert.acsinte@gmail.com>
2024-10-04 20:07:29 +0200

    added snapshot for bike station page

---
+++ src/app/bikeStations/page.test.tsx
@@ -0,0 +1,52 @@
+import { afterAll, afterEach, beforeAll, expect, it, } from 'vitest';
+import { render } from "@testing-library/react";
+import Bikes from './page';
+import { setupServer } from 'msw/node'
+import { HttpResponse, http } from 'msw'
+
+
+const station = [
+    {
+        results: [
+            {
+                id: 1,
+                name: 'Station 1',
+                bikes_in_use: 2,
+                bikes_available: 3
+            }
+        ]
+    },
+    {
+        results: [
+            {
+                id: 2,
+                name: 'Station 2',
+                bikes_in_use: 7,
+                bikes_available: 7
+            }
+        ]
+    }
+]
+  
+export const restHandlers = [
+http.get('https://data.stad.gent/api/explore/v2.1/catalog/datasets/blue-bike-deelfietsen-gent-dampoort/records?select=id%2C%20name%2C%20bikes_in_use%2C%20bikes_available', () => {
+    return HttpResponse.json(station[0])
+}),
+http.get('https://data.stad.gent/api/explore/v2.1/catalog/datasets/blue-bike-deelfietsen-gent-sint-pieters-m-hendrikaplein/records?select=id%2C%20name%2C%20bikes_in_use%2C%20bikes_available', () => {
+    return HttpResponse.json(station[1])
+}),
+]
+
+const server = setupServer(...restHandlers)
+
+beforeAll(() => server.listen())
+
+afterAll(() => server.close())
+
+afterEach(() => server.resetHandlers())
+
+it('Snapshot Bike Station Page', async () => {
+    const jsx = await Bikes();
+    const result = render(jsx)
+    expect(result).toMatchSnapshot()
+})
