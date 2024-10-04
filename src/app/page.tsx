bdd87736487a3dd445448800f9c27813d796d9b2
Robert Acsinte <robert.acsinte@gmail.com>
2024-10-04 17:41:32 +0200

    Link instead of button for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBicycle, faSquareParking } from "@fortawesome/free-solid-svg-icons";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="wrapper-center">
         <div className="card-grid">
-        <button className="card" aria-label="Link to the page about bikes">
-          <FontAwesomeIcon icon={faBicycle} className="icon" aria-hidden="true"/>
-        </button>
-        <button className="card" aria-label="Link to the page about parking">
-          <FontAwesomeIcon icon={faSquareParking} className="icon" aria-hidden="true"/>
-        </button>
+          <Link href="/bikeStations" className="card" aria-label="Link to the page about bikes">
+            <FontAwesomeIcon icon={faBicycle} className="icon" aria-hidden="true"/>
+          </Link>
+          <Link href="/bikeStations" className="card" aria-label="Link to the page about parking">
+            <FontAwesomeIcon icon={faSquareParking} className="icon" aria-hidden="true"/>
+          </Link>
       </div>
     </main>
   );
