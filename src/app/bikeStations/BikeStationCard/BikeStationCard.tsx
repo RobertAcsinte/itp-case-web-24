70dfc7280fc805a3200f5e22b802fd2d69a685ec
Robert Acsinte <robert.acsinte@gmail.com>
2024-10-04 17:57:36 +0200

    style changes for bike station card

--- src/app/bikeStations/BikeStationCard/BikeStationCard.tsx
+++ src/app/bikeStations/BikeStationCard/BikeStationCard.tsx
@@ -8,11 +8,12 @@ type Station = {
 }
 
 export default function BikeStationCard({station}: {station: Station}) {
+    
     return (
         <div className={style.container}>
             <h2>{station.name}</h2>
-            <p>Bikes available: {station.bikes_available}</p>
-            <p>Total bikes: {station.bikes_in_use + station.bikes_available}</p>
+            <p><b>Bikes available:</b> {station.bikes_available}</p>
+            <p><b>Total bikes:</b> {station.bikes_in_use + station.bikes_available}</p>
         </div>
     );
 }
