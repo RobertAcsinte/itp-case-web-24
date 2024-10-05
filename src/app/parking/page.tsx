fe0523751fcee7c08444624558221d2471d340a1
Robert Acsinte <robert.acsinte@gmail.com>
2024-10-05 23:14:46 +0200

    fetch parkings from endpoint

---
+++ src/app/parking/page.tsx
@@ -0,0 +1,49 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { getData } from "../utils/networkRequests";
+
+type parking = {
+    results: {
+        name: string;
+        totalcapacity: number;
+        availablecapacity: number;
+        occupation: number;
+        isopennow: number,
+        categorie: string,
+        description: string,
+        location: {
+            lon: number,
+            lat: number
+        }
+    }[];
+  }
+
+  const parkings = [
+    'https://data.stad.gent/api/explore/v2.1/catalog/datasets/bezetting-parkeergarages-real-time/records?select=name%2C%20totalcapacity%2C%20availablecapacity%2C%20occupation%2C%20isopennow%2C%20categorie%2C%20description%2C%20location&where=availablecapacity%20%3E%3D%20(totalcapacity%20%2F%202)&order_by=totalcapacity%20DESC'
+  ]
+
+export default async function Page () {
+    const data = await Promise.all(
+        parkings.map(async (api) => {
+          const station = await getData<parking>(api);
+          return station;
+        })
+      );
+
+    return (
+        <main className="wrapper">
+            <div className="title-container">
+                <Link href="/" aria-label="Link to go back to the home page">
+                    <FontAwesomeIcon icon={faArrowLeft} aria-hidden="true" />
+                </Link>
+                <h1>Parkings in Ghent</h1>
+                {data.map((parkings) => (
+                    parkings.results.map((parkingInfo) => (
+                        <div>{parkingInfo.name}</div>
+                    ))
+                ))}
+            </div>
+        </main>
+    )
+};
