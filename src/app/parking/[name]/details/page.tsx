76cd2907b2acaee10927d0b0ccfce3855f44ab9b
Robert Acsinte <robert.acsinte@gmail.com>
2024-10-06 18:22:05 +0200

    improved accesbility page parking details

--- src/app/parking/[name]/details/page.tsx
+++ src/app/parking/[name]/details/page.tsx
@@ -13,29 +13,29 @@ export default async function Page(searchParams: { params: { name: string } } ) 
     return (
         <main className="wrapper">
             <div className="navbar-title">
-            <Link href="/parking" aria-label="Link to go back to the list of parkings">
+                <Link href="/parking" aria-label="Link to go back to the list of parkings">
                     <FontAwesomeIcon icon={faArrowLeft} aria-hidden="true" />
-            </Link>
-            <h1>{parkingDetails.name}</h1>
-            </div>
-            <section>
-            <div className="card">
-                <p><b>Category:</b> {parkingDetails.categorie}</p>
-                <p><b>Description:</b> {parkingDetails.description}</p>
-                <p><b>Available spots:</b> {parkingDetails.availablecapacity}</p>
-                <p><b>Percentage of taken spots from total spots:</b> {((parkingDetails.occupation / parkingDetails.totalcapacity) * 100).toFixed(2)}%</p>
-                <Link
-                    href={`https://www.google.com/maps/search/?api=1&query=${parkingDetails.location.lat}%2C${parkingDetails.location.lon}`}
-                    aria-label="Link to go to Google Maps" 
-                    className="details-link"
-                    target="_blank" 
-                    rel="noopener noreferrer" 
-                >
-                    Open in Google Maps
                 </Link>
+                <h1 id="parking-name">{parkingDetails.name}</h1>
             </div>
-        </section>
+            <section aria-labelledby="parking-name">
+                <div className="card">
+                    <h2>Parking Information</h2>
+                    <p><b>Category:</b> {parkingDetails.categorie}</p>
+                    <p><b>Description:</b> {parkingDetails.description}</p>
+                    <p><b>Available spots:</b> {parkingDetails.availablecapacity}</p>
+                    <p><b>Percentage of taken spots from total spots:</b> {((parkingDetails.occupation / parkingDetails.totalcapacity) * 100).toFixed(2)}%</p>
+                    <Link
+                        href={`https://www.google.com/maps/search/?api=1&query=${parkingDetails.location.lat}%2C${parkingDetails.location.lon}`}
+                        aria-label="Link to go to Google Maps" 
+                        className="details-link"
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                    >
+                        Open in Google Maps
+                    </Link>
+                </div>
+            </section>
         </main>
-
     )
 }
