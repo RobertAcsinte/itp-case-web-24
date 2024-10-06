0c2331f1f0c994318cca8cfcfda19760b2f3f5f0
Robert Acsinte <robert.acsinte@gmail.com>
2024-10-06 02:58:13 +0200

    added google maps button for parking details page

--- src/app/parking/[name]/details/page.tsx
+++ src/app/parking/[name]/details/page.tsx
@@ -13,7 +13,7 @@ export default async function Page(searchParams: { params: { name: string } } ) 
     return (
         <main className="wrapper">
             <div className="navbar-title">
-            <Link href="/" aria-label="Link to go back to the list of parkings">
+            <Link href="/parking" aria-label="Link to go back to the list of parkings">
                     <FontAwesomeIcon icon={faArrowLeft} aria-hidden="true" />
             </Link>
             <h1>{parkingDetails.name}</h1>
@@ -24,6 +24,15 @@ export default async function Page(searchParams: { params: { name: string } } ) 
                 <p><b>Description:</b> {parkingDetails.description}</p>
                 <p><b>Available spots:</b> {parkingDetails.availablecapacity}</p>
                 <p><b>Percentage of taken spots from total spots:</b> {((parkingDetails.occupation / parkingDetails.totalcapacity) * 100).toFixed(2)}%</p>
+                <Link
+                    href={`https://www.google.com/maps/search/?api=1&query=${parkingDetails.location.lat}%2C${parkingDetails.location.lon}`}
+                    aria-label="Link to go to Google Maps" 
+                    className="details-link"
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                >
+                    Open in Google Maps
+                </Link>
             </div>
         </section>
         </main>
